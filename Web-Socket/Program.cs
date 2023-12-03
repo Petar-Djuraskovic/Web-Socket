7470277679471b66a66edfc80fc9e25f3de2c97c
Petar Đurašković <petardjura288@gmail.com>
2023-12-03 14:52:26 +0100

    event system WIP (last update before introducing server objects)

--- Web-Socket/Program.cs
+++ Web-Socket/Program.cs
@@ -1,12 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
+using Newtonsoft.Json;
+
+
 
 class Program
 {
     private static TcpListener server = new TcpListener(IPAddress.Any, 48163);
+
+    private static List<TcpClient>? ClientList = new List<TcpClient>();
+
+    // Define event args for server updates
+
+    public class ServerEventArgs : EventArgs
+    {
+        public string UpdateMessage { get; set; }
+        
+        public ServerEventArgs(string updateMessage)
+        {
+            UpdateMessage = updateMessage;
+        }
+    }
+
+    public class MessageUpdateArgs : EventArgs
+    {
+        public string UpdateMessage { get; set; }
+        public TcpClient TcpClient { get; }
+        public DateTime DateTime { get; }
+        public string DisplayTime { get; }
+
+        public MessageUpdateArgs(string message, TcpClient tcpClient, DateTime dateTime)
+        {
+            UpdateMessage = message;
+            TcpClient = tcpClient;
+            DateTime = dateTime;
+            DisplayTime = dateTime.ToString("H:mm");
+        }
+    }
+
+    // Define an event for server updates
+    public static event EventHandler<ServerEventArgs>? ServerUpdate;
+    public static event EventHandler<MessageUpdateArgs>? MessageUpdate;
+
+    // Method to trigger the MessageUpdate event
+    protected static async Task OnMessageUpdate(MessageUpdateArgs e)
+    {
+        MessageUpdate?.Invoke(null, e);
+
+        // Convert MessageUpdateArgs to JSON
+        string jsonMessage = JsonConvert.SerializeObject(e);
+
+        // Send the JSON message over the WebSocket
+        await SendUpdateToClients(jsonMessage);
+    }
+
+
+    private static async Task SendUpdateToClients(string jsonMessage)
+    {
+        foreach (var client in ClientList)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(jsonMessage);
+                await stream.WriteAsync(data, 0, data.Length);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($" | ! | SocketException while sending update to {client.Client.RemoteEndPoint}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" | ! | Exception while sending update to {client.Client.RemoteEndPoint}: {ex.Message}");
+            }
+        }
+    }
+
 
     static void Main(string[] args)
     {
@@ -28,7 +104,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("Invalid role. Please enter 'server' or 'client'.");
+                Console.WriteLine(" | № | Invalid role. Please enter 'server' or 'client'.");
                 Main(args);
             }
         }
@@ -43,7 +119,7 @@ class Program
         IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
 
         // Display the IP addresses in the console
-        Console.WriteLine("////////// role : server //////////");
+        Console.WriteLine("/////////֎ role : server ֎/////////");
         Console.WriteLine("Server IP addresses:");
         foreach (IPAddress ipAddress in localIPs)
         {
@@ -52,7 +128,12 @@ class Program
                 Console.WriteLine($"  {ipAddress}");
             }
         }
+
         Console.WriteLine("Server running . . .");
+
+        // Simulate a server update and notify clients
+        //OnServerUpdate(new ServerEventArgs("Server is now running."));
+
         Console.ReadLine();
     }
 
@@ -76,7 +157,9 @@ class Program
         byte[] message = new byte[4096];
         int bytesRead;
 
-        Console.WriteLine($"Client connected: {tcpClient.Client.RemoteEndPoint}");
+        ClientList.Add(tcpClient);
+
+        Console.WriteLine($" | ☺ | Client connected: {tcpClient.Client.RemoteEndPoint}");
 
         while (true)
         {
@@ -86,9 +169,15 @@ class Program
             {
                 bytesRead = clientStream.Read(message, 0, 4096);
             }
-            catch
+            catch (SocketException ex)
             {
-                break; 
+                Console.WriteLine($" | ! | SocketException while reading from {tcpClient.Client.RemoteEndPoint}: {ex.Message}");
+                break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" | ! | Exception while reading from {tcpClient.Client.RemoteEndPoint}: {ex.Message}");
+                break;
             }
 
             if (bytesRead == 0)
@@ -98,35 +187,87 @@ class Program
             Console.WriteLine($"Received message from {tcpClient.Client.RemoteEndPoint}: {clientMessage}");
         }
 
+        ClientList.Remove(tcpClient);
+
         Console.WriteLine($"Client disconnected: {tcpClient.Client.RemoteEndPoint}");
+
+        // Simulate a server update and notify clients
+        //OnServerUpdate(new ServerEventArgs($"Client {tcpClient.Client.RemoteEndPoint} disconnected."));
 
         tcpClient.Close();
     }
 
     static void StartClient()
     {
-        Console.WriteLine("////////// role : client /////////");
-        Console.Write("Enter the server IP address: ");
-        string? ipAddress = Console.ReadLine(); // Mark as nullable if needed
+        Console.WriteLine("/////////֎ role : client ֎////////");
 
-        if (ipAddress != null) // Check for null before dereferencing
+        TryConnectingToASpecifiedServer();
+
+        static void TryConnectingToASpecifiedServer()
         {
-            TcpClient client = new TcpClient(ipAddress, 48163);
-            NetworkStream stream = client.GetStream();
+            Console.Write("Enter the server IP address: (press enter to connect to self)");
+            string? ipAddress = Console.ReadLine(); // Mark as nullable if needed
 
-            Console.WriteLine("Connected to server.");
-
-            while (true)
+            if (ipAddress != null) // Check for null before dereferencing
             {
-                Console.Write("Enter a message: ");
-                string? message = Console.ReadLine(); // Mark as nullable if needed
 
-                if (message != null) // Check for null before dereferencing
+                TryConnectingAgain(ipAddress);
+
+                static void TryConnectingAgain(string? ipAddress)
                 {
-                    byte[] data = Encoding.UTF8.GetBytes(message);
-                    stream.Write(data, 0, data.Length);
+
+                    try
+                    {
+                        TcpClient client = new TcpClient(ipAddress, 48163);
+                        NetworkStream stream = client.GetStream();
+
+                        Console.WriteLine("Connected to server.");
+
+                        while (true)
+                        {
+                            Console.Write("Enter a message: ");
+                            string? message = Console.ReadLine(); // Mark as nullable if needed
+
+                            if (message != null) // Check for null before dereferencing
+                            {
+                                byte[] data = Encoding.UTF8.GetBytes(message);
+                                stream.Write(data, 0, data.Length);
+                            }
+                        }
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine($" | ! | SocketException while connecting to the server: {ex.Message}");
+
+                        TryConnectingToASpecifiedServer();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($" | ! | Exception while connecting to the server: {ex.Message}");
+                        TryConnectingToASpecifiedServer();
+                    }
                 }
             }
+            else
+            {
+                Console.WriteLine("Invalid IP address.");
+                TryConnectingToASpecifiedServer();
+            }
+        }
+
+        static void AskoToTryConnectingAgain()
+        {
+            Console.WriteLine("try again? (y/n)");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                //tryconnectingagain();
+            }
+        }
+
+        // Method to trigger the ServerUpdate event
+        static void OnServerUpdate(ServerEventArgs e)
+        {
+            ServerUpdate?.Invoke(null, e);
         }
     }
 }
